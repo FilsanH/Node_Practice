cadf489d6eb803407def530d8fee70da52e9d20c
Filsan Hassan <fh363.cam.ac.uk>
2020-08-28 11:27:39 +0100

    add body parser

--- SimpleCrudApp/server.js
+++ SimpleCrudApp/server.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
-//create and listen to server 
+//Place bodyParser before CRUD
+// this converts the data in the form element to something that is readable and adds it to the req.body
+app.use(bodyParser.urlencoded({ extended: true }))
 
-
-app.get('/', (req, res){
- res.send.dFile
-
-)}
-app.listen(3000, ()=>{
- console.log('listenting on 3000')
+//All routes
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
 })
 
+app.post('/quotes', (req, res) => {
+  console.log('form submitted')
+  console.log(req.body)
+})
+
+app.listen(3000, () => {
+  console.log('listenting on 3000')
+})
