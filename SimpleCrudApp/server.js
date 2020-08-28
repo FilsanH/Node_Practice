38a3d3b3739cf4d029927b77675dafd64bf792b0
Filsan Hassan <fh363.cam.ac.uk>
2020-08-28 11:13:00 +0100

    Simple CRUD App

---
+++ SimpleCrudApp/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+//create and listen to server 
+
+
+app.get('/', (req, res){
+ res.send.dFile
+
+)}
+app.listen(3000, ()=>{
+ console.log('listenting on 3000')
+})
+
