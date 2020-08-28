be94eb94be287fb3255188dcf92451eb6ea586c5
Filsan Hassan <fh363.cam.ac.uk>
2020-08-28 11:54:14 +0100

    simple connect to db

--- SimpleCrudApp/server.js
+++ SimpleCrudApp/server.js
@@ -2,6 +2,39 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionString = `mongodb+srv://dev:filsanhassan@cluster1.iojny.mongodb.net/starWars?retryWrites=true&w=majority`
+//using callbacks :
+/*
+MongoClient.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+    const db = client.db()
+    console.log(db)
+  }
+) */
+// using promises
+//split into two one connects to client the other returns db and thefore lets you change dbs 
+
+let _db 
+MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log('Connected to Database')
+    // The database
+     _db = client.db()
+    // create a collection
+    const quotesCollection = db.collection('quotes')
+  }
+)
+
 //Place bodyParser before CRUD
 // this converts the data in the form element to something that is readable and adds it to the req.body
 app.use(bodyParser.urlencoded({ extended: true }))
